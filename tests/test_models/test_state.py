c4b1e3d0f296c08a99aa71b5495cefc91d248a3f
shincap8 <shincap8@gmail.com>
2020-04-03 02:02:24 +0000

    Remove the changes done

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -54,15 +54,6 @@ class TestState(unittest.TestCase):
         self.assertEqual(type(self.state.name), str)
 
     @unittest.skipIf(
-        os.getenv('HBNB_TYPE_STORAGE') != 'db',
-        "This test only work in Filestorage")
-    def test_State_in_db(self):
-        """test state"""
-        state = State(name="Pittsburgh")
-        if state.id in models.storage.all():
-            self.assertTrue(state.name, "Pittsburgh")
-
-    @unittest.skipIf(
         os.getenv('HBNB_TYPE_STORAGE') == 'db',
         "This test only work in Filestorage")
     def test_save_State(self):
